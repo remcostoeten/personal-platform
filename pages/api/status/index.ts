feeed8ab2492a973c80ea9628fd4cef36adb6a41
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-07 18:12:25 +0200

    api design

--- pages/api/status/index.ts
+++ pages/api/status/index.ts
@@ -1,4 +1,4 @@
-import { Builder, By, until } from 'selenium-webdriver';
+import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
 import path from 'path';
 import fs from 'fs';
@@ -8,7 +8,7 @@ import { getCurrentDateTime } from '@/core/helpers/getCurrentDateTime';
 interface StatusObject {
   name: string;
   status: string;
-  timestamp: string; // Changed type from Date to string
+  timestamp: string;
   onlinefor: string | null;
   offlineSince: string | null;
   lastSeen: string | null;
@@ -21,8 +21,12 @@ let previousStatus: string | null = null;
 let statusChangedAt: number | null = null;
 let timesOnline: number = 0;
 let firstSeen: Date | string | null = null;
+let lastSeen: Date |string | null = null;
 let totalOnlineDuration = 0;
+let lastOnlineTimestamp = null;
 let totalOfflineDuration = 0;
+let lastOfflineTimestamp = null;
+const ITTERATION_DURATION = 2500;
 
 async function writeStatusesToFile(statuses: StatusObject[]) {
   const fileContent = `
@@ -50,7 +54,7 @@ async function writeStatusesToFile(statuses: StatusObject[]) {
 
 export default async (req: Request, res: Response): Promise<void> => {
   const time = getCurrentDateTime().time;
-  const timestamp = time
+  const timestamp = time;
 
   try {
     const name: string = (req.query.name as string) || (process.env.WHATSAPP_NAME as string);
@@ -75,29 +79,42 @@ export default async (req: Request, res: Response): Promise<void> => {
           console.log(`Finding and clicking element for ${name}`);
           let element = await driver.wait(
             until.elementLocated(By.xpath(`//span[contains(text(), '${name}')]`)),
-            2500
+            ITTERATION_DURATION
           );
           await element.click();
 
           console.log('Getting status');
 
-          let currentStatus = await driver.findElement(By.xpath("//span[@title='Online']"))
-            .then(() => "Online")
-            .catch(() => "Offline");
-
-            if (previousStatus !== currentStatus) {
-              if (previousStatus === "Offline" && currentStatus === "Online") {
-                timesOnline++;
-                firstSeen = timestamp;
-              }
-              statusChangedAt = Date.now();
+          let currentStatus;
+          try {
+            await driver.findElement(By.xpath("//span[@title='Online']"));
+            currentStatus = "Online";
+            if (lastOnlineTimestamp) {
+              const now = new Date();
+              totalOnlineDuration += Math.floor((now.getTime() - lastOnlineTimestamp.getTime()) / 1000);
             }
+            lastOnlineTimestamp = new Date();
+          } catch (error) {
+            currentStatus = "Offline";
+            if (lastOfflineTimestamp) {
+              const now = new Date();
+              totalOfflineDuration += Math.floor((now.getTime() - lastOfflineTimestamp.getTime()) / 1000);
+            }
+            lastOfflineTimestamp = new Date();
+          }
 
-            const duration = statusChangedAt ? Math.floor((Date.now() - statusChangedAt) / 1000) : 0;
+          // Increment timesOnline if status changes from "Offline" to "Online"
+          if (previousStatus === "Offline" && currentStatus === "Online") {
+            timesOnline++;
+          }
+
+          // Update previousStatus
+          previousStatus = currentStatus;
+
           const statusObject: StatusObject = {
             name,
             status: currentStatus,
-            timestamp: timestamp, // Assigning timestamp value
+            timestamp: timestamp,
             onlinefor: currentStatus === "Online" ? `${totalOnlineDuration} seconds` : null,
             offlineSince: currentStatus === "Offline" ? `${totalOfflineDuration} seconds` : null,
             lastSeen: timestamp,
@@ -105,20 +122,13 @@ export default async (req: Request, res: Response): Promise<void> => {
             firstSeen
           };
 
-          if (currentStatus === "Online") {
-            totalOnlineDuration += duration;
-          } else {
-            totalOfflineDuration += duration;
-          }
-
-
           statusData.push(statusObject);
           console.log(`Status for ${name}: ${statusObject.status}`);
           console.log(JSON.stringify(statusObject));
 
           writeStatusesToFile(statusData);
 
-          await new Promise(resolve => setTimeout(resolve, 2500));
+          await new Promise(resolve => setTimeout(resolve, ITTERATION_DURATION));
         } catch (error) {
           console.error('An error occurred:', error);
         }
