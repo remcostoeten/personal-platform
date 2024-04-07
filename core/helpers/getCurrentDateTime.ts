e8ac056b61ded22fdbeda2d4f0ef9d87358cfc83
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-07 15:56:38 +0200

    Work on api design + create date helper

---
+++ core/helpers/getCurrentDateTime.ts
@@ -0,0 +1,7 @@
+export const getCurrentDateTime = () => {
+    let today = new Date();
+    let date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    let time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
+    return { date, time };
+  };
