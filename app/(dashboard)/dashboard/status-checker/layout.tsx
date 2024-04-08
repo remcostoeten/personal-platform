ffa44b25786f2edd284fc08600d2124187756c8b
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-08 20:54:49 +0200

    wip

--- app/(dashboard)/dashboard/status-checker/layout.tsx
+++ app/(dashboard)/dashboard/status-checker/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import type { Metadata } from "next";
 
 export default function DashboardLayout({
   children,
--- components/overview.tsx
+++ components/overview.tsx
@@ -71,7 +71,7 @@ export function Overview() {
           axisLine={false}
           tickFormatter={(value) => `$${value}`}
         />
-        <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+        <Bar dataKey="total" fill="#02c9a5" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   );
