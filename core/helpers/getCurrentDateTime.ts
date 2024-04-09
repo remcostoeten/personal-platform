11e797b4518e7cd8064c0b8aa2b125ca0613ea69
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-09 21:25:05 +0200

    wip

--- core/helpers/getCurrentDateTime.ts
+++
@@ -1,9 +0,0 @@
-export const getCurrentDateTime = () => {
-  let today = new Date();
-  let date = `${today.getFullYear()}-${
-    today.getMonth() + 1
-  }-${today.getDate()}`;
-  let time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-
-  return { isoString: today.toISOString(), date, time };
-};
