560e77d3cfc01944f25a893d7d47887ae340b1a6
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-09 20:52:50 +0200

    api wip

--- components/ chat/ActivityMonitor.tsx
+++ components/ chat/ActivityMonitor.tsx
@@ -1,42 +1,30 @@
 "use client";
+import { StatusObject } from "@/statusData";
 import React from "react";
 
-interface StatusData {
-  name: string;
-  status: string;
-  timestamp: string;
-  onlinefor: string;
-  offlineSince: string | null;
-  lastSeen: string;
-  timesOnline: number;
-  FirstTimeOnline: string;
-  ts: string;
-  lastSessionDuration: string;
-}
-
 interface ActivityMonitorProps {
-  StatusData: StatusData;
+    StatusData: StatusObject;
 }
 
 const ActivityMonitor: React.FC<ActivityMonitorProps> = ({ StatusData }) => {
-  const {
-    name,
-    status,
-    lastSeen,
-    timesOnline,
-    firstSeen,
-    lastSessionDuration,
-  } = StatusData;
+    const {
+        name,
+        status,
+        lastSeen,
+        timesOnline,
+        firstTimestamp,
+        lastSessionDuration,
+    } = StatusData;
 
-  return (
-    <div>
-      <p>
-        {name} is currently {status}. Last seen at {lastSeen}. The last online
-        session lasted {lastSessionDuration} seconds. A total of {timesOnline}{" "}
-        online sessions since {firstSeen}.
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <p>
+                {name ? name : "no data"} is currently {status ? status : "no data"}. Last seen at {lastSeen ? lastSeen : "no data"}. The last online
+                session lasted {lastSessionDuration ? lastSessionDuration : "no data"} seconds. A total of {timesOnline ? timesOnline : "no data"}{" "}
+                online session since <span className="font-semibold">{firstTimestamp ? firstTimestamp : "no data"}</span>.
+            </p>
+        </div>
+    );
 };
 
 export default ActivityMonitor;
