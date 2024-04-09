38cc0610a54b5d264423e9957a3b93c48ef1ab7d
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-10 00:42:11 +0200

    pass prop in online indiciator

--- components/ chat/config.ts
+++ components/ chat/config.ts
@@ -1,6 +1,6 @@
 const ITTERATION_DURATION = 1100;
 const RESULTS_PER_PAGE = 10;
 const CHROME_PROFILE_PATH = "../../../../../chromeprofile";
-const SCRAPE_URL = 'https://status.remcostoeten.com/'
+const SCRAPE_URL = 'https://web.whatsapp.com/'
 
 export { ITTERATION_DURATION, RESULTS_PER_PAGE, CHROME_PROFILE_PATH, SCRAPE_URL };
